397199020377cc58aa55d4b4b76f5e21fee9d545
Agnieszka Graban <agnieszka.ziomkowska@gmail.com>
2024-06-12 22:26:27 +0200

    Add second text

--- my_project/src/components/Title/Title.js
+++ my_project/src/components/Title/Title.js
@@ -3,10 +3,14 @@ import './Title.css'
 
 const Title = () => {
     const [text, setText] = useState('Witaj!')
+    const [question, setQuestion] = useState('Wybierz profil lub dodaj nowy')
 
     return (
         <div>
-            <div className='title'><h1 className="title__text">{text}</h1></div>
+            <div className='title'>
+                <h1 className="title__text">{text}</h1>
+                <h2 className="title__question">{question}</h2>
+            </div>
         </div>
     );
 };
