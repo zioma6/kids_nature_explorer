2717741a16671962063846c9fc66549793aaf718
Agnieszka Graban <agnieszka.ziomkowska@gmail.com>
2024-06-24 11:11:02 +0200

    Revert "Merge pull request #8 from zioma6/Pytanie_do_Roberta"
    This reverts commit 222e5a68d3a5808d274bc47a294d3a200684cc12, reversing
    changes made to 6de68b447213e4adc3957273d5da9e672003f406.

--- my_project/src/components/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import journalReducer from './journalSlice';
-
-export const store = configureStore({
-    reducer: {
-        journal: journalReducer,
-    },
-});
